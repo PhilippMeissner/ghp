a3564bd9d5ff19f041851d4327ebb2b556ab0e43
Philipp Meißner <pm@zeit.io>
2015-04-13 12:34:42 +0200

    finished until task 9

--- labor1/app/src/main/java/com/example/climax/ghp_versuch1/MainActivity.java
+++ labor1/app/src/main/java/com/example/climax/ghp_versuch1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.climax.ghp_versuch1;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +9,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private SharedPreferences prefs;
     Button btn_ok;
     private final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop wurde aufgerufen.");
+
+        EditText text = (EditText) findViewById(R.id.editText);
+        prefs = (SharedPreferences) text.getText();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +42,60 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
 
                 // LogCat Ausgabe erzeugen
-                Log.d(TAG, "Button \"btn_ok\" wurde betätigt");
+                Log.d(TAG, "Button \"btn_ok\" wurde betätigt.");
 
                 //Toast ausgeben
                 Context context = getApplicationContext();
-                CharSequence text = "@string/toast_text";
+
+                // Text aus string.xml laden
+                CharSequence text =  getString(R.string.toast_text);
                 int duration = Toast.LENGTH_SHORT;
 
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
             }
         });
+
+        // Get Layout the button shall be added to
+        //LinearLayout curr_layout = (LinearLayout) View.inflate(this, R.layout.activity_main, null);
+
+        LinearLayout curr_layout = (LinearLayout) findViewById(R.id.linear_layout);
+        Button dyn_btn = new Button(this);
+
+        dyn_btn.setText(R.string.dyn_btn_text);
+
+        //dyn_btn.setLayoutParams(curr_layout.getLayoutParams());
+        dyn_btn.setLayoutParams(findViewById(R.id.ok_btn).getLayoutParams());
+
+        dyn_btn.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // LogCat Ausgabe erzeugen
+                Log.d(TAG, "Button \"dyn_btn\" wurde betätigt.");
+
+                //Toast ausgeben
+                Context context = getApplicationContext();
+
+                // Text aus string.xml laden
+                CharSequence text =  getString(R.string.dyn_toast_text);
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+
+
+        });
+
+
+        curr_layout.addView(dyn_btn);
+
+        // Add prefs to editText
+        // TODO - NOT WORKING
+        EditText edit_field =  (EditText) findViewById(R.id.editText);
+        SharedPreferences.Editor editor = prefs.edit();
     }
 
 
